1bd2a542084d8517608eef0cc3e34e2335d8b89d
Devin <devhunter16@gmail.com>
2023-07-12 08:56:21 -0700

    Passed the data from trips.json to the travel.hbs page using our travel controller.

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,15 @@
-/* Rendering the travel page */
+// Requiring the built-in Node.js file system so we can retrieve the JSON data in trips.json
+const fileSystem = require('fs');
+
+// Storing our parsed JSON data in the "trips" variable
+const trips = JSON.parse(fileSystem.readFileSync('./data/trips.json', 'utf8'));
+
+/* 
+* Rendering the travel page (Passing our "trips" variable through so that we can use
+* the data stored within in our travel.hbs page
+*/
 const travel = (request, response) => {
-    response.render('travel', { title: 'Travlr Getaways' });
+    response.render('travel', { title: 'Travlr Getaways', trips });
 };
 
 module.exports = {
