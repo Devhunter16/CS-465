8d6c1693382512dfbaaf9b49784e30b979efe0f9
Devin <devhunter16@gmail.com>
2023-07-18 08:30:33 -0700

    Added mongoose and defined a schema for a collection we'll be calling trips.

---
+++ app_server/models/travlr.js
@@ -0,0 +1,21 @@
+// Importing mongoose, an ODM (object data modeling) library for MongoDB and Node.js
+const mongoose = require('mongoose');
+
+// Defining our trip schema class. This defines the structure of the documents that will
+// be stored in the "trips" collection in our DB. We have a "code" and "name" here so 
+// that we can find and retrieve trips faster and more efficiently from MongoDB.
+const tripSchema = new mongoose.Schema({
+    code: { type: String, required: true, index: true },
+    name: { type: String, required: true, index: true },
+    length: { type: String, required: true },
+    start: { type: Date, required: true },
+    resort: { type: String, required: true },
+    perPerson: { type: String, required: true },
+    image: { type: String, required: true },
+    description: { type: String, required: true }
+});
+
+// Our collection in MongDB will be named 'trips', as we defined in the first argument. 
+// The second argument, "tripSchema" is the schema that defines the structure for 
+// documents in the trips collection.
+mongoose.model('trips', tripSchema);
