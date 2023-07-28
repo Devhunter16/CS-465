5ceb4c0f53095169c46ff59be1706910e1f566cd
Devin <devhunter16@gmail.com>
2023-07-28 11:17:50 -0700

    Added another router.

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -7,7 +7,11 @@ const tripsController = require("../controllers/trips");
 // When a GET request comes in, we pass it to the trips controller and use the tripsList
 // function we defined in the "trips.js" controller file
 router
-  .route("/trips")
-  .get(tripsController.tripsList)
+    .route('/trips')
+    .get(tripsController.tripsList)
+
+router
+    .route('/trips/:tripCode')
+    .get(tripsController.tripsFindCode);
 
 module.exports = router;
