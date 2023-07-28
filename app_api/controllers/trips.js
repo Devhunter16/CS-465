55f27ad9fda8ca8e57b97ff307a324f2e7b6207b
Devin <devhunter16@gmail.com>
2023-07-28 10:39:55 -0700

    Edited some comments.

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -7,17 +7,22 @@ const tripsList = async (req, res) => {
     Model
         .find({}) // Finding all trips
         .exec((err, trips) => {
-            if (!trips) { // If we do not find anything
+            if (!trips) { // If we do not find anything (null, nothing was returned)
                 return res
                     .status(404) // Return "not found" 404 status 
+                    // This is an API, so even the error messages need to be formatted
+                    // as JSON
                     .json({ "message": "trips not found" });
             } else if (err) {
                 return res 
                     .status(404)
+                    // Taking the error block and turning it into JSON
                     .json(err);
             } else { // If we found what we needed, send client HTTP 200 success status
                 return res
                     .status(200)
+                    // Taking trips data that was returned from Mongoose and rendering it
+                    // as JSON
                     .json(trips);
             }
         });
