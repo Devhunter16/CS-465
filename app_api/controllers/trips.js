7a9bf750728534f315e269098ce8a83a4e55b60a
Devin <devhunter16@gmail.com>
2023-07-28 11:10:25 -0700

    Updated trips.js file so that we can go to localhost:3000/api/trips and see our JSON data there.

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -4,50 +4,36 @@ const Model = mongoose.model('trips');
 
 // Async function that gets trips from the db
 const tripsList = async (req, res) => {
-    Model
-        .find({}) // Finding all trips
-        .exec((err, trips) => {
-            if (!trips) { // If we do not find anything (null, nothing was returned)
-                return res
-                    .status(404) // Return "not found" 404 status 
-                    // This is an API, so even the error messages need to be formatted
-                    // as JSON
-                    .json({ "message": "trips not found" });
-            } else if (err) {
-                return res 
-                    .status(404)
-                    // Taking the error block and turning it into JSON
-                    .json(err);
-            } else { // If we found what we needed, send client HTTP 200 success status
-                return res
-                    .status(200)
-                    // Taking trips data that was returned from Mongoose and rendering it
-                    // as JSON
-                    .json(trips);
-            }
-        });
-};
+    try {
+      // Finding all trips then executing the code below
+      const trips = await Model.find({}).exec(); 
+      // If we do not find anything
+      if (!trips || trips.length === 0) { 
+        // Return "not found" 404 status
+        return res.status(404).json({ "message": "trips not found" }); 
+      } else { 
+        // If we found what we needed, send client HTTP 200 success status
+        return res.status(200).json(trips);
+      }
+    } catch (err) {
+      // This is an API, so even the error messages need to be formatted as JSON
+      return res.status(500).json(err);
+    }
+  };
 
 // Async function that gets a trip from the db based on it's code
 const tripsFindCode = async (req, res) => {
-    Model
-        .find({ 'code': req.params.tripCode }) // Finding trip based on it's code
-        .exec((err, trip) => {
-            if (!trip) { // If we do not find anything
-                return res
-                    .status(404) // Return "not found" 404 status 
-                    .json({ "message": "trip not found" });
-            } else if (err) {
-                return res 
-                    .status(404)
-                    .json(err);
-            } else { // If we found what we needed, send client HTTP 200 success status
-                return res
-                    .status(200)
-                    .json(trip);
-            }
-        });
-};
+    try {
+      const trip = await Model.find({ 'code': req.params.tripCode }).exec();
+      if (!trip || trip.length === 0) {
+        return res.status(404).json({ "message": "trip not found" });
+      } else {
+        return res.status(200).json(trip);
+      }
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  };
 
 // Exporting our functions
 module.exports = {
