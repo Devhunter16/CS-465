2b416a18f8bfab35fc20da8db5b2802dcae4322b
Devin <devhunter16@gmail.com>
2023-08-01 08:47:16 -0700

    Updated index.js.

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -4,8 +4,6 @@ const router = express.Router();
 // Importing the functions we defined in the "trips.js" controller file
 const tripsController = require("../controllers/trips");
 
-// When a GET request comes in, we pass it to the trips controller and use the tripsList
-// function we defined in the "trips.js" controller file
 router
     .route('/trips')
     .get(tripsController.tripsList)
