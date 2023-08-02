abb64d9816ca505198b3fc238424c3eb5ce61f7a
Devin <devhunter16@gmail.com>
2023-08-02 11:24:12 -0700

    Further working toward CRUD functionality in the SPA admin site.

--- app_admin/services/trip-data.service.ts
+++ app_admin/services/trip-data.service.ts
@@ -11,6 +11,7 @@ export class TripDataService {
   constructor(private http: Http) { }
 
   private apiBaseUrl = 'http://localhost:3000/api/';
+  private tripUrl = `${this.apiBaseUrl}trips/`;
 
   public addTrip(formData: Trip): Promise<Trip> {
     console.log('Inside TripDataService#addTrips');
@@ -21,6 +22,15 @@ export class TripDataService {
       .catch(this.handleError);
   } 
 
+  public getTrip(tripCode: string): Promise<Trip> {
+    console.log('Inside TripDataService#getTrip(tripCode)');
+    return this.http
+      .get(this.tripUrl + tripCode)
+      .toPromise()
+      .then(response => response.json() as Trip)
+      .catch(this.handleError);
+   }
+
   public getTrips(): Promise<Trip[]> {
     console.log('Inside TripDataService#getTrips');
     return this.http
@@ -29,6 +39,16 @@ export class TripDataService {
       .then(response => response.json() as Trip[])
       .catch(this.handleError);
   }
+
+  public updateTrip(formData: Trip): Promise<Trip> {
+    console.log('Inside TripDataService#upateTrip');
+    console.log(formData);
+    return this.http
+      .put(this.tripUrl + formData.code, formData)
+      .toPromise()
+      .then(response => response.json() as Trip[])
+      .catch(this.handleError);
+   }
 
   private handleError(error: any): Promise<any> {
     console.error('Something has gone wrong', error);
