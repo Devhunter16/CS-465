19cd5b00884fe74defc2eee1f10e5a1c833a163f
Devin <devhunter16@gmail.com>
2023-08-09 09:04:41 -0700

    Setting up User model.

--- app_api/database/db.js
+++ app_api/database/db.js
@@ -30,33 +30,33 @@ mongoose.connection.on('disconnected', () => {
 });
 
 // Closes the Mongoose connection
-const gracefulShutdown = (msg, callback) => {               
-    mongoose.connection.close( () => {                        
-        console.log(`Mongoose disconnected through ${msg}`);    
-        callback();                                             
-    });                                                       
-};    
-                                                      
+const gracefulShutdown = (msg, callback) => {
+    mongoose.connection.close(() => {
+        console.log(`Mongoose disconnected through ${msg}`);
+        callback();
+    });
+};
+
 // For nodemon restarts                                     
-process.once('SIGUSR2', () => {                             
-    gracefulShutdown('nodemon restart', () => {               
-        process.kill(process.pid, 'SIGUSR2');                   
-    });                                                       
-});   
+process.once('SIGUSR2', () => {
+    gracefulShutdown('nodemon restart', () => {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
 
 // For app termination                                      
-process.on('SIGINT', () => {                                
-    gracefulShutdown('app termination', () => {               
-        process.exit(0);                                        
-    });                                                       
+process.on('SIGINT', () => {
+    gracefulShutdown('app termination', () => {
+        process.exit(0);
+    });
 });
 
 // For Heroku app termination                               
-process.on('SIGTERM', () => {                               
-    gracefulShutdown('Heroku app shutdown', () => {           
-        process.exit(0);                                        
-    });                                                       
-});  
+process.on('SIGTERM', () => {
+    gracefulShutdown('Heroku app shutdown', () => {
+        process.exit(0);
+    });
+});
 
 // Calling connect()
 connect();
